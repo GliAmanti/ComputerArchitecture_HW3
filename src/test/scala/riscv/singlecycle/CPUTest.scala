1161072e0a465c6a2a2618345824ffcc6e8a0235
Sally Lu <s305hueyi@gmail.com>
2023-11-29 01:58:43 +0800

    my assembly file (v3) my asmbin my CPUTest

--- src/test/scala/riscv/singlecycle/CPUTest.scala
+++ src/test/scala/riscv/singlecycle/CPUTest.scala
@@ -113,3 +113,28 @@ class ByteAccessTest extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 }
+
+class HammingTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior.of("Single Cycle CPU")
+  it should "caculate the hamming distance between two 64-bit integers" in {
+    test(new TestTopModule("myHammingDist.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
+      for (i <- 1 to 50) {
+        c.clock.step(1000)
+        c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
+      }
+
+      c.io.mem_debug_read_address.poke(4.U)
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(21.U)
+
+      c.io.mem_debug_read_address.poke(8.U) 
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(63.U)
+
+      c.io.mem_debug_read_address.poke(12.U) 
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(0.U)
+    }
+  }
+}
+
